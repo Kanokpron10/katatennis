df7146c4cd4a3cba4a7d5a951484f99d90bfa419
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-19 14:26:00 +0700

    [Edited]: แก้ไขให้ผ่าน Player1 score เป็น 1 Player2 score เป็น 0 ที่ไฟล์ resultgame.go

--- resultgame.go
+++ resultgame.go
@@ -1,5 +1,12 @@
 package katatennis
 
 func ResultGame(player1score, player2score int) string {
-	return "LOVE-LOVE"
+	mapResult := map[int]string{
+		0: "LOVE",
+		1: "15",
+		2: "30",
+		3: "40",
+	}
+
+	return mapResult[player1score] + "-" + mapResult[player2score]
 }
