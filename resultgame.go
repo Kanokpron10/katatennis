026068522f16bb4b9bceac94fd14033eea891dcc
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-19 17:21:33 +0700

    [Added]: เพิ่มฟังก์ชัน AddScore ที่ไฟล์ resultgame.go

--- resultgame.go
+++ resultgame.go
@@ -1,6 +1,21 @@
 package katatennis
 
-func ResultGame(player1score, player2score int) string {
+const RESULTWIN = 4
+
+type PlayerScore struct {
+	Player1score int
+	Player2score int
+}
+
+func (playerscore *PlayerScore) AddScore(player int) {
+	if player == 1 {
+		playerscore.Player1score++
+	} else {
+		playerscore.Player2score++
+	}
+}
+
+func (score PlayerScore) ResultGame() string {
 	mapResult := map[int]string{
 		0: "LOVE",
 		1: "15",
@@ -8,13 +23,15 @@ func ResultGame(player1score, player2score int) string {
 		3: "40",
 	}
 
-	if player1score == 4 {
+	if score.Player1score == RESULTWIN || score.Player2score == RESULTWIN {
+		return checkWin(score.Player1score)
+	}
+	return mapResult[score.Player1score] + "-" + mapResult[score.Player2score]
+}
+
+func checkWin(Player1score int) string {
+	if Player1score == RESULTWIN {
 		return "Player 1 Win"
 	}
-
-	if player2score == 4 {
-		return "Player 2 Win"
-	}
-
-	return mapResult[player1score] + "-" + mapResult[player2score]
+	return "Player 2 Win"
 }
